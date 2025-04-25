8ce2c4bd7f504f3f119691d04860935954b52a5e
Swaraj Mohaptra <110276137+swarajcodes@users.noreply.github.com>
2025-04-25 23:13:39 +0530

    prodrelease 2.0

--- lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primaryColor = Colors.lightGreenAccent;
-}
-
-class AppStrings {
-  static const appName = "Katha Webtoons";
-}
